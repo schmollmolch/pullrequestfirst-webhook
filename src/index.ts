6bb08cccf02fe6b8ef1b16e4aa13799bf376e190
Christian Scheja <christian@scheja.me>
2019-06-29 17:05:01 +0200

    feat: checking for open issue before referencing it in pull request

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,63 @@
-import { Application } from 'probot'
+import { debug } from 'debug'
+import { Application, Context } from 'probot'
+import { WebhookPayloadPush } from '@octokit/webhooks'
+
+const log = debug('create-pullrequest-webhook')
+debug.enable('create-pullrequest-webhook');
+
+const createPullRequestForBranch = (context: Context, branchName: string) => {
+  const newPullRequest = context.issue({ title: 'Merging ' + branchName, head: branchName, base: 'master', draft: true })
+  log(`Creating pull request ${JSON.stringify(newPullRequest)} for branch ${branchName}`)
+  return context.github.pullRequests.create(newPullRequest)
+}
+
+const createPullRequestForIssue = (context: Context, issueNumber: number, branchName: string) => {
+  const newPullRequest = context.issue({ issue: issueNumber, head: branchName, base: 'master', draft: true })
+  log(`Creating pull request ${JSON.stringify(newPullRequest)} for issue ${issueNumber}`)
+  return context.github.pullRequests.createFromIssue(newPullRequest)
+}
 
 export = (app: Application) => {
   app.on('push', async (context) => {
-    const match = context.payload.ref.match(/^refs\/heads\/(\w*)$/)
-    if (match !== null && match[1] !== 'master') {
-      const branchName = match[1];
+    log(`============== New Request ==============`)
+    log(`Received push to git ref ${context.payload.ref}`)
 
-      const existingPullRequests = await context.github.pullRequests.list(context.issue(({ state: 'open', head: branchName, base: 'master' })));
+    // check if push git ref was a branch that is not master, see https://developer.github.com/v3/git/refs/
+    const gitRefMatch = (context.payload as WebhookPayloadPush).ref.match(/^refs\/heads\/([\w-]*)$/)
+    if (gitRefMatch !== null && gitRefMatch[1] !== 'master') {
+      const branchName = gitRefMatch[1]
+      log(`Figured out branch name ${branchName}`)
 
+      // check if branch is not yet covered by an open pull request]
+      const existingPullRequests = await context.github.pullRequests.list(context.issue(({ state: 'open', head: branchName, base: 'master' })))
       if (existingPullRequests.data && existingPullRequests.data.length === 0) {
-        const newPullRequest = context.issue({ title: 'Merging ' + branchName, head: branchName, base: 'master', draft: true })
-        await context.github.pullRequests.create(newPullRequest)
+        log(`No open pull request found for this branch`)
+
+        // check if branch contains numbers. if true, search for corresponding issue. if still open, create pull request for issue
+        const issueNumberMatch = branchName.match(/\D*(\d{1,}).*/)
+        if (issueNumberMatch !== null) {
+          const issueNumber = parseInt(issueNumberMatch[1])
+          log(`Branch name contains number ${issueNumber}, try to find matching issue`)
+
+          try {
+            const issue = await context.github.issues.get(context.issue({ number: issueNumber }))
+            if (issue.status === 200 && issue.data.state === 'open') {
+              log(`Found issue number ${issueNumber} and it's open.`)
+              await createPullRequestForIssue(context, issueNumber, branchName)
+            } else {
+              log(`Found issue number ${issueNumber} but it's not open.`)
+              await createPullRequestForBranch(context, branchName)
+            }
+          } catch (err) {
+            // 404 when issue not found throws error. Going to create pull request for the branch then.
+            log(`Could not find issue number ${issueNumber}: ${err}. `)
+            await createPullRequestForBranch(context, branchName)
+          }
+        } else {
+          log(`Branch name does not contains numbers.`)
+          await createPullRequestForBranch(context, branchName)
+        }
       }
     }
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,15 @@
-// You can import your modules
-// import index from '../src/index'
-
 import nock from 'nock'
 // Requiring our app implementation
 import myProbotApp from '../src'
 import { Probot } from 'probot'
 // Requiring our fixtures
-import push_to_tag from './fixtures/push.tag.json'
-import push_to_master from './fixtures/push.master.json'
-import push_to_branch_issue from './fixtures/push.branch.issue.json'
-import push_to_branch_noissue from './fixtures/push.branch.json'
-import get_pullrequest_exists from './fixtures/get.pullrequest.existing.json';
+import pushToTag from './fixtures/push.tag.json'
+import pushToMaster from './fixtures/push.master.json'
+import pushToBranchIssue from './fixtures/push.branch.issue.json'
+import pushToBranchNoissue from './fixtures/push.branch.json'
+import getPullrequestExists from './fixtures/get.pullrequest.existing.json'
+import getIssueOpen from './fixtures/get.issue.open.json'
+import getIssueClosed from './fixtures/get.issue.closed.json'
 
 const pullRequestCreatedForIssueBody = {
   issue: 123,
@@ -22,6 +21,13 @@ const pullRequestCreatedForIssueBody = {
 const pullRequestCreatedForBranchBody = {
   title: 'Merging hotfix_urgent_urgent',
   head: 'hotfix_urgent_urgent',
+  base: 'master',
+  draft: true
+}
+
+const pullRequestCreatedForClosedIssueBody = {
+  title: 'Merging issue-123',
+  head: 'issue-123',
   base: 'master',
   draft: true
 }
@@ -46,92 +52,150 @@ describe('Pull Request First', () => {
     // nock expects nothing
 
     // Receive a webhook event
-    await probot.receive({ name: 'push', payload: push_to_tag })
-    done();
+    await probot.receive({ name: 'push', payload: pushToTag })
+    done()
   })
 
   test('does nothing when a push happens on master', async (done) => {
     // nock expects nothing
 
     // Receive a webhook event
-    await probot.receive({ name: 'push', payload: push_to_master });
-    done();
+    await probot.receive({ name: 'push', payload: pushToMaster })
+    done()
   })
 
   test('does nothing if existing pull request when an push happens on a branch', async (done) => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' });
+      .reply(200, { token: 'test' })
 
     // Test query for existing pull request
     nock('https://api.github.com')
       .get('/repos/hiimbex/testing-things/pulls')
       .query(query => {
-        //{ state: 'open', head: 'hotfix_urgent_urgent', base: 'master' }
-        done(expect(query).toMatchObject({ state: 'open', head: 'hotfix_urgent_urgent', base: 'master' }))
-        return true;
+        expect(query).toMatchObject({ state: 'open', head: 'hotfix_urgent_urgent', base: 'master' })
+        return true
       })
-      .reply(200, get_pullrequest_exists);
+      .reply(200, getPullrequestExists)
 
     // Receive a webhook event
-    await probot.receive({ name: 'push', payload: push_to_branch_noissue });
-  });
+    await probot.receive({ name: 'push', payload: pushToBranchNoissue })
+    done()
+  })
 
   test('create pull request when an push happens on a branch without pull request without issue id in name', async (done) => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' });
+      .reply(200, { token: 'test' })
 
     // Test query for existing pull request
     nock('https://api.github.com')
       .get('/repos/hiimbex/testing-things/pulls')
       .query(() => true)
-      .reply(200, []);
+      .reply(200, [])
 
     // create pull request for branch
     nock('https://api.github.com')
       .post('/repos/hiimbex/testing-things/pulls', (body: any) => {
-        done(expect(body).toMatchObject(pullRequestCreatedForBranchBody))
-        return true;
+        expect(body).toMatchObject(pullRequestCreatedForBranchBody)
+        return true
       })
-      .reply(200);
+      .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'push', payload: push_to_branch_noissue });
-  });
+    await probot.receive({ name: 'push', payload: pushToBranchNoissue })
+    done()
+  })
 
-  test('create pull request when an push happens on a branch without pull request with issue id in name', async (done) => {
+  test('create pull request when an push happens on a branch without pull request with issue id in name and issue does not exist', async (done) => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' });
+      .reply(200, { token: 'test' })
 
     // Test query for existing pull request
     nock('https://api.github.com')
       .get('/repos/hiimbex/testing-things/pulls')
       .query(() => true)
-      .reply(200, []);
+      .reply(200, [])
+
+    // Test query for existing issue
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/issues/123')
+      .reply(404, 'not found')
 
     // create pull request for branch
     nock('https://api.github.com')
       .post('/repos/hiimbex/testing-things/pulls', (body: any) => {
-        done(expect(body).toMatchObject(pullRequestCreatedForIssueBody))
-        return true;
+        expect(body).toMatchObject(pullRequestCreatedForClosedIssueBody)
+        return true
       })
-      .reply(200);
+      .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'push', payload: push_to_branch_issue });
-  });
+    await probot.receive({ name: 'push', payload: pushToBranchIssue })
+    done()
+  })
+
+  test('create pull request when an push happens on a branch without pull request with issue id in name and issue is closed', async (done) => {
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' })
+
+    // Test query for existing pull request
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/pulls')
+      .query(() => true)
+      .reply(200, [])
+
+    // Test query for existing issue
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/issues/123')
+      .reply(200, getIssueClosed)
+
+    // create pull request for branch
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/pulls', (body: any) => {
+        expect(body).toMatchObject(pullRequestCreatedForClosedIssueBody)
+        return true
+      })
+      .reply(200)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'push', payload: pushToBranchIssue })
+    done()
+  })
+
+  test('create pull request when an push happens on a branch without pull request with issue id in name and issue is open', async (done) => {
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' })
+
+    // Test query for existing pull request
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/pulls')
+      .query(() => true)
+      .reply(200, [])
+
+    // Test query for existing issue
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/issues/123')
+      .reply(200, getIssueOpen)
+
+    // create pull request for branch
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/pulls', (body: any) => {
+        expect(body).toMatchObject(pullRequestCreatedForIssueBody)
+        return true
+      })
+      .reply(200)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'push', payload: pushToBranchIssue })
+    done()
+  })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
